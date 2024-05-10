9e36e4867ae6f39363d27b2b53e406eb81e37ad3
Carl Walsh <darthwalsh@gmail.com>
2024-05-10 08:27:44 -0700

    Remove newlines inside paragraphs
    Paragraph line wrap was different which github diff doesn't know how to deal with (unlike wdiff or git diff --word-dif

--- build.ps1
+++ build.ps1
@@ -17,4 +17,5 @@ Get-Content -Raw $txt
   | sed -e 's/[“”]/"/g' -e "s/[‘’]/'/g" # Normalize smart quotes
   | sed -e "s//→/g" # Normalize arrows
   | sed -e "s//-/g" # Normalize bullets
+  | sed --null-data --regexp-extended "s/\n([a-z])/ \1/g" # Remove newline before lowercase
   | Set-Content norm.txt -Encoding 'UTF-8'
